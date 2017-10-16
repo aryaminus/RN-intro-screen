0db6ccf88b08346f00b19b6075012e440f7f0036
Sunim Acharya <sunim.54@gmail.com>
2017-10-16 23:28:56 +0545

    Setup for intro screen

---
+++ app/components/Button.js
@@ -0,0 +1,41 @@
+/**
+ * Button component
+ * Renders a button and calls a function passed via onPress prop once tapped
+ */
+
+import React, { Component } from "react";
+import {
+  StyleSheet, // CSS-like styles
+  Text, // Renders text
+  TouchableOpacity, // Pressable container
+  View // Container component
+} from "react-native";
+
+export default class Button1 extends Component {
+  render({ onPress } = this.props) {
+    return (
+      <TouchableOpacity onPress={onPress}>
+        <View style={styles.button}>
+          <Text style={styles.text}>{this.props.text.toUpperCase()}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // Button container
+  button: {
+    borderRadius: 50, // Rounded border
+    borderWidth: 2, // 2 point border widht
+    borderColor: "#FFFFFF", // White colored border
+    paddingHorizontal: 50, // Horizontal padding
+    paddingVertical: 10 // Vertical padding
+  },
+  // Button text
+  text: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontFamily: "Avenir"
+  }
+});
---
+++ app/components/Screen.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet, // CSS-like styles
+  Text, // Renders text
+  View // Container component
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import Button from "./Button";
+import Swiper from "./Swiper";
+import { StackNavigator } from "react-navigation";
+export default class Screen extends Component {
+  render() {
+    return (
+      <Swiper navigation={this.props.navigation}>
+        {/* First screen */}
+        <View style={[styles.slide, { backgroundColor: "#16a085" }]}>
+          <Icon name="ios-home" {...iconStyles} />
+          <Text style={styles.header}>one</Text>
+          <Text style={styles.text}>one</Text>
+        </View>
+        {/* Second screen */}
+        <View style={[styles.slide, { backgroundColor: "#16a085" }]}>
+          <Icon name="ios-people" {...iconStyles} />
+          <Text style={styles.header}>two</Text>
+          <Text style={styles.text}>two</Text>
+        </View>
+        {/* Third screen */}
+        <View style={[styles.slide, { backgroundColor: "#16a085" }]}>
+          <Icon name="ios-videocam" {...iconStyles} />
+          <Text style={styles.header}>three</Text>
+          <Text style={styles.text}>three</Text>
+        </View>
+      </Swiper>
+    );
+  }
+}
+const iconStyles = {
+  size: 100,
+  color: "#FFFFFF"
+};
+const styles = StyleSheet.create({
+  // Slide styles
+  slide: {
+    flex: 1, // Take up all screen
+    justifyContent: "center", // Center vertically
+    alignItems: "center" // Center horizontally
+  },
+  // Header styles
+  header: {
+    color: "#FFFFFF",
+    fontFamily: "Avenir",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginVertical: 15
+  },
+  // Text below header
+  text: {
+    color: "#FFFFFF",
+    fontFamily: "Avenir",
+    fontSize: 18,
+    marginHorizontal: 40,
+    textAlign: "center"
+  }
+});
+AppRegistry.registerComponent("Screen", () => Screen);
