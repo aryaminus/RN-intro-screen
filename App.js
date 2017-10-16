6d2682208625d67e9ebb7380006559ceed11de99
Sunim Acharya <sunim.54@gmail.com>
2017-10-16 23:23:10 +0545

    Setup for navigation

--- App.js
+++ App.js
@@ -4,54 +4,66 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { StackNavigator } from "react-navigation";
+
+import Screen from "./app/components/Screen";
+import Swiper from "./app/components/Swiper";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-export default class App extends Component<{}> {
+class Homescreen extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Screen navigation={this.props.navigation} />
       </View>
     );
   }
 }
 
+const App = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: "Welcome"
+    }
+  },
+  Screen: {
+    screen: Screen,
+    navigationOptions: {
+      title: "Screen"
+    }
+  },
+  Swiper: {
+    screen: Swiper,
+    navigationOptions: {
+      title: "Swiper"
+    }
+  }
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
