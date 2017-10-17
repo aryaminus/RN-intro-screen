9d0fafe213e81f6fc7ab19b0662eaed2933b6653
Sunim Acharya <sunim.54@gmail.com>
2017-10-17 09:07:42 +0545

    Make changes to Statusbar and back button in Boiler

--- app/components/Boiler.js
+++ app/components/Boiler.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
 //import { StackNavigator } from "react-navigation";
 export default class Boiler extends Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "#16a085",
+      elevation: null
+    },
+    headerLeft: null
+  };
   render() {
     return <Text>OLA</Text>;
   }
