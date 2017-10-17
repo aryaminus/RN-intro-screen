90a7ed8406a4a40ce4a4bfc076628475e1ac7657
Sunim Acharya <sunim.54@gmail.com>
2017-10-17 09:01:14 +0545

    Setup for intro

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ class Home extends Component<{}> {
   }
 }
 
-const App = StackNavigator({
+export default App = StackNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
--- app/components/Boiler.js
+++ app/components/Boiler.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
-
+//import { StackNavigator } from "react-navigation";
 export default class Boiler extends Component {
   render() {
     return <Text>OLA</Text>;
